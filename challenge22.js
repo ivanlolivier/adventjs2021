e55b6ce7bc9829da437ff62d6f49315f01af935e
Ivan L'olivier <ivan.lolivier@gmail.com>
2022-11-11 18:04:08 -0300

    adding all missing challenges

---
+++ challenge22.js
@@ -0,0 +1,9 @@
+export default function countDecorations(bigTree) {
+  if (bigTree == null) {
+    return 0;
+  }
+
+  return bigTree.value
+    + countDecorations(bigTree.left)
+    + countDecorations(bigTree.right);
+}
